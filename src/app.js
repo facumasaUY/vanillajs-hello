e2f9903968bb70340694ab6e334abc5574edd3a5
facumasaUY <facundomasa@gmail.com>
2024-09-23 22:26:21 +0000

    Muchas gracias a mis compañeros y profesores

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function randomItem(array) {
+    let numeroAleatorio = Math.random() * array.length;
+    let numeroEntero = Math.floor(numeroAleatorio);
+    return array[numeroEntero];
+  }
+
+  let excuse =
+    randomItem(who) +
+    " " +
+    randomItem(action) +
+    " " +
+    randomItem(what) +
+    " " +
+    randomItem(when);
+  console.log(excuse);
+
+  document.querySelector("#excuse").innerHTML = excuse;
 };
